08e65f77e451091a493d450954ed54d675f8fd5b
Tresh4IOS <402473245@qq.com>
2017-04-17 13:42:58 +0800

    增加了一些文件
    增加了一些define 文件增加了可用性

---
+++ Utils/IETColordefines.h
@@ -0,0 +1,30 @@
+//
+//  IETColordefines.h
+//  TreshKit
+//
+//  Created by cc on 2017/4/17.
+//  Copyright © 2017年 Tresh. All rights reserved.
+//
+
+#ifndef IETColordefines_h
+#define IETColordefines_h
+
+
+#define RGB(r,g,b) [UIColor colorWithRed:r/256.0 green:g/256.0 blue:b/256.0 alpha:1]
+#define RGBA(r,g,b,a) [UIColor colorWithRed:r/256.0 green:g/256.0 blue:b/256.0 alpha:a]
+
+
+#define COLOR_COMMON_TABLE_BACK RGB(242,242,242)
+#define COLOR_COMMON_VC_BACK RGB(247,247,247)
+#define COLOR_COMMON_SEPARATER RGB(216,216,216)
+#define COLOR_COMMON_TEXT_GRAY RGB(80,80,80)
+#define COLOR_COMMON_TEXT_LIGHTGRAY RGB(158,158,158)
+#define COLOR_COMMON_TEXT_BLUE RGB(68,87,154)
+#define COLOR_COMMON_TEXT_LIGHTBLUE RGB(88,147,245)
+#define COLOR_COMMON_TEXT_RED RGB(250,55,55)
+#define COLOR_COMMON_TEXT_ORANGE RGB(240,146,72)
+#define COLOR_COMMON_BORDOR_GRAY RGB(158,158,158)
+#define COLOR_COMMON_BLUE RGB(68,87,154)
+#define COLOR_COMMON_LIGHTBLUE RGB(91,149,242)
+#define COLOR_COMMON_CELLBACK_GRAY RGB(249,249,249)
+#endif /* IETColordefines_h */
---
+++ Utils/IETFontdefines.h
@@ -0,0 +1,13 @@
+//
+//  IETFontdefines.h
+//  TreshKit
+//
+//  Created by cc on 2017/4/17.
+//  Copyright © 2017年 Tresh. All rights reserved.
+//
+
+#ifndef IETFontdefines_h
+#define IETFontdefines_h
+
+#define SYS_FONT(s) [UIFont systemFontOfSize:s]
+#endif /* IETFontdefines_h */
